ce951537db3114f3dfd14f7e5af8432bb4b992f4
e.muchasaj <e.muchasaj@gmail.com>
2024-08-23 20:45:45 +0200

    optimized image banner

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardBody, Heading, Image, HStack } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import Emoji from './Emoji';
+import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
     game: Game
@@ -12,7 +13,7 @@ interface Props {
 const GameCard: React.FC<Props> = ({ game }: Props) => {
     return (
         <Card>
-            <Image src={game.background_image} alt={game.name} />
+            <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
             <CardBody>
                 <HStack justifyContent='space-between' marginBottom={3}>
                     <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
