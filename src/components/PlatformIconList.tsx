06c18f966a33cd5f30291f051646c6a07968c6f0
e.muchasaj <e.muchasaj@gmail.com>
2024-08-24 17:50:04 +0200

    import fixes

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
-import { Platform } from '../hooks/useGames';
+import { Platform } from '../hooks/usePlatforms';
 import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa';
 import { MdPhoneIphone } from 'react-icons/md';
 import { SiNintendo } from 'react-icons/si';
