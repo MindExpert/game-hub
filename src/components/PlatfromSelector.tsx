6bc4b769fad88f0cbbf05d2a056d4acd9efd6a47
e-muchasaj <e.muchasaj@7btc.co>
2024-08-29 11:10:01 +0200

    Create lookup hooks

--- src/components/PlatfromSelector.tsx
+++ src/components/PlatfromSelector.tsx
@@ -2,6 +2,7 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
 import Platform from "../entities/Platform";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
@@ -13,7 +14,7 @@ const PlatformSelector = ({
   selectedPlatformId
 }: Props) => {
   const { data: platforms, error } = usePlatforms();
-  const selectedPlatform = platforms?.results.find(p => p.id === selectedPlatformId);
+  const selectedPlatform = usePlatform(selectedPlatformId);
 
   if (error) return null;
 
